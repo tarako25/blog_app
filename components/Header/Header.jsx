b0ffef67830dae062bab1bf588a14af0cada035f
muckman <takenoko1603@gmail.com>
2023-02-24 08:23:57 +0900

    2023/02/24_マージ修正後

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,15 +1,11 @@
 import Link from "next/link"
 import styles from "./Header.module.css"
-<<<<<<< HEAD
 import Image from "next/image"
 import logo from "@/img/logo.png"
-=======
->>>>>>> b2b26b628cd295710b553581303eb41d5945f1b2
 
 export function Header(){
     return(
         <div className={styles.header}>
-<<<<<<< HEAD
             <div className={styles.logo}>
             <Link href="../">
                 <Image src={logo} className={styles.logoimg}/>
@@ -28,16 +24,6 @@ export function Header(){
                     </Link>
                     <Link href="../contact">
                         <li>お問い合わせ</li>
-=======
-            <div className={styles.logo}>タイトル</div>
-            <div className={styles.menus}>
-                <ul>
-                    <Link href="../">
-                        <li>トップ</li>
-                    </Link>
-                    <Link href="./Trial">
-                        <li>console.logテスト用</li>
->>>>>>> b2b26b628cd295710b553581303eb41d5945f1b2
                     </Link>
                 </ul>
             </div>
--- components/Main/Main.jsx
+++ components/Main/Main.jsx
@@ -1,16 +1,9 @@
 import styles from "./Main.module.css"
 import comon from "@/styles/Comon.module.css"
 import Link from "next/link"
-<<<<<<< HEAD
 import Image from "next/image"
 
 export function Main(props){
-=======
-import img from "@/img/img.jpg"
-import Image from "next/image"
-
-export function Main(){
->>>>>>> b2b26b628cd295710b553581303eb41d5945f1b2
     return(
             <div className={comon.all}>
                 
@@ -19,7 +12,6 @@ export function Main(){
                 {/* 新着記事 */}
                 <div className={comon.hl}>新着記事</div>
                 <div className={styles.newarticlebox}>
-<<<<<<< HEAD
 
                 {props.newarticles.map((newarticle)=>(
                     <Link href={newarticle.href}>
@@ -31,31 +23,12 @@ export function Main(){
                     </div>
                 </Link>
                 ))}
-=======
-                    <Link href="">
-                        <div className={styles.article}>
-                            <Image src={img} className={styles.img}/>
-                            <div className={styles.descriptionbox}>
-                                <div className={styles.description}>Lorem ipsum dolor sit amet, consectetur adipisicing.</div>
-                            </div>
-                        </div>
-                    </Link>
-                    <Link href="">
-                        <div className={styles.article}>
-                            <Image src={img} className={styles.img}/>
-                            <div className={styles.descriptionbox}>
-                                <div className={styles.description}>Lorem ipsum dolor sit amet, consectetur adipisicing.</div>
-                            </div>
-                        </div>
-                    </Link>
->>>>>>> b2b26b628cd295710b553581303eb41d5945f1b2
                 </div>
                 {/* /新着記事 */}
 
                 {/* 記事一覧 */}
                 <div className={comon.hl}>記事一覧</div>
                 <div className={styles.articlebox}>
-<<<<<<< HEAD
                 {props.articles.map((article)=>(
                     <Link href={article.href}>
                     <div className={styles.article}>
@@ -66,24 +39,6 @@ export function Main(){
                     </div>
                 </Link>
                 ))}
-=======
-                <Link href="">
-                        <div className={styles.article}>
-                            <Image src={img} className={styles.img}/>
-                            <div className={styles.descriptionbox}>
-                                <div className={styles.description}>Lorem ipsum dolor sit amet, consectetur adipisicing.</div>
-                            </div>
-                        </div>
-                    </Link>
-                    <Link href="">
-                        <div className={styles.article}>
-                            <Image src={img} className={styles.img}/>
-                            <div className={styles.descriptionbox}>
-                                <div className={styles.description}>Lorem ipsum dolor sit amet, consectetur adipisicing.</div>
-                            </div>
-                        </div>
-                    </Link>
->>>>>>> b2b26b628cd295710b553581303eb41d5945f1b2
                 </div>
                 {/* /記事一覧 */}
 
