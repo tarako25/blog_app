480eeb43d7a4b7020d898ff406b39aa941611c72
muckman <takenoko1603@gmail.com>
2023-02-24 14:44:16 +0900

    お試し

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import Link from "next/link"
 import styles from "components/Header/Header.module.css"
 import Image from "next/image"
-import logo from "img/Logo.png"
+import logo from "img/Twitter.png"
 
 export function Header(){
     return(
