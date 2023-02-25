be7a108b5e50b0183f9b63d943369e770bda8830
muckman <takenoko1603@gmail.com>
2023-02-26 06:51:10 +0900

    残りのレスポンス対応とハンバーガーメニューの追加

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -4,18 +4,20 @@ import Image from "next/image"
 import title from "img/Title.png"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { useState } from "react";
+
 
 export function Header(){
+
+    const [showMenu, setShowMenu] = useState(false);
+    const toggleMenu = () => {
+    setShowMenu(!showMenu);
+};
+
     return(
-        <div className={styles.header}>
-            <div className={styles.logo}>
-                <Link href="../">
-                    <Image src={title} alt="ロゴの画像" className={styles.logoimg}/>
-                </Link >
-                <FontAwesomeIcon icon={faBars} className={styles.fabars} />
-            </div>
-            <div className={styles.menus}>
-                <ul>
+        <div>
+             <div className={`${styles.hamburger} ${showMenu ? styles.show : styles.dis}`}>
+                <ul className={styles.hamul}>
                     <Link href="../">
                         <li>ホーム</li>
                     </Link>
@@ -29,6 +31,30 @@ export function Header(){
                         <li>お問い合わせ</li>
                     </Link>
                 </ul>
+            </div>
+            <div className={styles.header}>
+                <div className={styles.logo}>
+                    <Link href="../">
+                        <Image src={title} alt="ロゴの画像" className={styles.logoimg}/>
+                    </Link >
+                    <FontAwesomeIcon icon={faBars} className={styles.fabars} onClick={toggleMenu}/>
+                </div>
+                <div className={styles.menus}>
+                    <ul>
+                        <Link href="../">
+                            <li>ホーム</li>
+                        </Link>
+                        <Link href="../profile">
+                            <li>プロフィール</li>
+                        </Link>
+                        <Link href="../privacy">
+                            <li>プライバシーポリシー</li>
+                        </Link>
+                        <Link href="../contact">
+                            <li>お問い合わせ</li>
+                        </Link>
+                    </ul>
+                </div>
             </div>
         </div>
     )
