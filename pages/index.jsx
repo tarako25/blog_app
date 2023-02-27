9e5049af7281d66628854e9021dff198dc9b7f44
muckman <takenoko1603@gmail.com>
2023-02-27 12:57:44 +0900

    title変更

--- pages/index.jsx
+++ pages/index.jsx
@@ -8,6 +8,13 @@ import { Up_Side } from "components/Up_Side";
 export default function index(){
 
   return(
+    <>
+    <meta charset="UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="description" content="鯖の煮つけと麻婆豆腐炒めチャンネルブログについてのページです"/>
+      <meta name="keywords" content="自作ブログ, 鯖の煮つけと麻婆豆腐炒めチャンネルブログについて, ポテチ初心者"/>
+      <meta name="google-site-verification" content="yPWWkIUSplswGDurIkWPz5ooliChjglbdm-yFdbZ_Aw" />
     <div className={styles.all}>
       <Header />
       <Category /> 
@@ -20,5 +27,6 @@ export default function index(){
         </div>
       </div>
     </div>
+    </>
   )
 }
