8c8ca055c459ce806eb0a0b226b23bb65f01d75b
muckman <takenoko1603@gmail.com>
2023-02-27 12:59:29 +0900

    1

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 import { config } from '@fortawesome/fontawesome-svg-core';
-import Link from 'next/link';
 
 config.autoAddCss = false;
 
@@ -9,11 +8,6 @@ export default function Document() {
     <Html lang="ja">
       <Head>
       <title>Blog_sample</title>
-        <Link
-          href="https://use.fontawesome.com/releases/v5.15.3/css/all.css"
-          integrity="sha384-NMq7AxBKilDxwyRvamvX8Qz7VGPnYPfQj7Vwv+8WkfI+gsxjKewlZ00zvX8iDTTn"
-          crossOrigin="anonymous"
-          />
       </Head>
       <body>
         <Main />
