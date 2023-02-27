3e0e1dbc5b4080f5a374849a5088c5d62487946d
muckman <takenoko1603@gmail.com>
2023-02-27 13:35:00 +0900

    asd

--- pages/_document.js
+++ pages/_document.js
@@ -1,26 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 import { config } from '@fortawesome/fontawesome-svg-core';
-import Link from 'next/link';
 
 config.autoAddCss = false;
 
 export default function Document() {
   return (
     <Html lang="ja">
-      <Head>
-      <title>Blog_sample</title>
-      <meta charset="UTF-8" />
-      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta name="description" content="鯖の煮つけと麻婆豆腐炒めチャンネルブログについてのページです"/>
-      <meta name="keywords" content="自作ブログ, 鯖の煮つけと麻婆豆腐炒めチャンネルブログについて, ポテチ初心者"/>
-      <meta name="google-site-verification" content="yPWWkIUSplswGDurIkWPz5ooliChjglbdm-yFdbZ_Aw" />
-        <Link
-          href="https://use.fontawesome.com/releases/v5.15.3/css/all.css"
-          integrity="sha384-NMq7AxBKilDxwyRvamvX8Qz7VGPnYPfQj7Vwv+8WkfI+gsxjKewlZ00zvX8iDTTn"
-          crossOrigin="anonymous"
-          />
-      </Head>
+      <Head/>
       <body>
         <Main />
         <NextScript />
--- pages/index.jsx
+++ pages/index.jsx
@@ -3,22 +3,34 @@ import { Header } from "components/Header/Header";
 import styles from "styles/Home.module.css"
 import { Up_Top } from "components/UP_Top";
 import { Up_Side } from "components/Up_Side";
+import Head from "next/head";
 
 
 export default function index(){
 
   return(
-    <div className={styles.all}>
-      <Header />
-      <Category /> 
-      <div className={styles.inside}>
-        <div className={styles.center}>
-          <Up_Top />
-        </div>
-        <div className={styles.side}>
-          <Up_Side />
+    <>
+      <Head>
+        <title>Blog_sample</title>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="鯖の煮つけと麻婆豆腐炒めチャンネルブログについてのページです"/>
+        <meta name="keywords" content="自作ブログ, 鯖の煮つけと麻婆豆腐炒めチャンネルブログについて, ポテチ初心者"/>
+        <meta name="google-site-verification" content="yPWWkIUSplswGDurIkWPz5ooliChjglbdm-yFdbZ_Aw" />
+      </Head>
+      <div className={styles.all}>
+        <Header />
+        <Category /> 
+        <div className={styles.inside}>
+          <div className={styles.center}>
+            <Up_Top />
+          </div>
+          <div className={styles.side}>
+            <Up_Side />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
