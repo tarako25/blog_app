0b1a7d1e72d64655821c6066a8f53856a70a35d5
muckman <takenoko1603@gmail.com>
2023-02-27 13:25:25 +0900

    sda

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,13 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { config } from '@fortawesome/fontawesome-svg-core';
+import Link from 'next/link';
+
+config.autoAddCss = false;
 
 export default function Document() {
   return (
     <Html lang="ja">
-      <Head>
-      </Head>
+      <Head/>
       <body>
         <Main />
         <NextScript />
