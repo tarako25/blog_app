553d2d0a0623a5dac1abf94f3187fdc98128cd75
muckman <takenoko1603@gmail.com>
2023-02-27 14:48:58 +0900

    ヘスンポシカニア.keyword

--- pages/index.jsx
+++ pages/index.jsx
@@ -10,12 +10,12 @@ export default function index(){
   return(
     <>
     <Head>
-        <title>Blog_sample</title>
+        <title>ヘスンポシカニア</title>
         <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content="鯖の煮つけと麻婆豆腐炒めチャンネルブログについてのページです"/>
-        <meta name="keywords" content="自作ブログ, 鯖の煮つけと麻婆豆腐炒めチャンネルブログについて, ポテチ初心者"/>
+        <meta name="description" content="ヘスンポシカニアについて"/>
+        <meta name="keywords" content="ヘスンポシカニア, 鯖の煮つけと麻婆豆腐炒めチャンネルブログについて, ポテチ初心者"/>
         <meta name="google-site-verification" content="yPWWkIUSplswGDurIkWPz5ooliChjglbdm-yFdbZ_Aw" />
       </Head>
       <div className={styles.all}>
