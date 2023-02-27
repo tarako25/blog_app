d061b45af803aa160641dc43f722e26ff7859be0
muckman <takenoko1603@gmail.com>
2023-02-27 13:29:57 +0900

    わからない

--- pages/index.jsx
+++ pages/index.jsx
@@ -8,26 +8,17 @@ import { Up_Side } from "components/Up_Side";
 export default function index(){
 
   return(
-    <>
-      <Head>
-      <title>Blog_sample</title>
-      <meta charset="UTF-8" />
-      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta name="description" content="鯖の煮つけと麻婆豆腐炒めチャンネルブログについてのページです"/>
-      </Head>
-      <div className={styles.all}>
-        <Header />
-        <Category /> 
-        <div className={styles.inside}>
-          <div className={styles.center}>
-            <Up_Top />
-          </div>
-          <div className={styles.side}>
-            <Up_Side />
-          </div>
+    <div className={styles.all}>
+      <Header />
+      <Category /> 
+      <div className={styles.inside}>
+        <div className={styles.center}>
+          <Up_Top />
+        </div>
+        <div className={styles.side}>
+          <Up_Side />
         </div>
       </div>
-    </>
+    </div>
   )
 }
