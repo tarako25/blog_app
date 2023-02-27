e39bfef2350061f7ce23517e9392bb1127b45c9b
muckman <takenoko1603@gmail.com>
2023-02-27 14:09:03 +0900

    imoprt head書き忘れ

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import 'styles/globals.css'
 import '@fortawesome/fontawesome-svg-core/styles.css';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
   <Head>
