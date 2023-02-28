2bbfe7c1f836675a672987a4f652f5b8308dd990
muckman <takenoko1603@gmail.com>
2023-02-28 13:14:20 +0900

    タイトル

--- pages/index.jsx
+++ pages/index.jsx
@@ -11,7 +11,7 @@ export default function index(){
   return(
     <>
     <Head>
-        <title>ヘスンポシカニア</title>
+        <title>Blog_sample</title>
         <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
