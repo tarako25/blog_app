8a08736f95609e2c02c9e8f8d804268ab16df12c
muckman <takenoko1603@gmail.com>
2023-02-28 09:14:58 +0900

    タイトル変更

--- pages/index.jsx
+++ pages/index.jsx
@@ -15,7 +15,7 @@ export default function index(){
         <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content="ヘスンポシカニアについて"/>
+        <meta name="description" content="Blog_sample"/>
         <meta name="keywords" content="ヘスンポシカニア, 鯖の煮つけと麻婆豆腐炒めチャンネルブログについて, ポテチ初心者"/>
         <meta name="google-site-verification" content="yPWWkIUSplswGDurIkWPz5ooliChjglbdm-yFdbZ_Aw" />
       </Head>
