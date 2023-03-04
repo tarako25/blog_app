6264816ade9d218ade19febf713cea3f26b3f909
muckman <takenoko1603@gmail.com>
2023-03-04 20:38:02 +0900

    seo対策

--- pages/index.jsx
+++ pages/index.jsx
@@ -15,8 +15,8 @@ export default function index(){
         <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content="Blog_sample"/>
-        <meta name="keywords" content="ヘスンポシカニア, 鯖の煮つけと麻婆豆腐炒めチャンネルブログについて, ポテチ初心者"/>
+        <meta name="description" content="素人が練習で作成したブログ形式のページです。"/>
+        <meta name="keywords" content="Blog_sample,サンプルブログ,Blog sample"/>
         <meta name="google-site-verification" content="yPWWkIUSplswGDurIkWPz5ooliChjglbdm-yFdbZ_Aw" />
       </Head>
       <div className={styles.all}>
